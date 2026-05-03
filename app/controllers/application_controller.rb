93e3283b839306aaaf8ae68dbc7d54c748b972a8
Nkanyiso Mzobe <104601010+NkanyisoMz@users.noreply.github.com>
2026-05-03 20:13:52 +0200

    feat: implement multi-tenancy with team-based data scoping

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+
+    before_action :authenticate_user!
+
+    def current_team
+        @current_team ||= current_user.teams.find(params[:team_id]) if params[:team_id]
+    end
 end
