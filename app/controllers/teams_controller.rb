58ae5a5412b8f8cd8b3ca17d92f37c84ebc04157
Nkanyiso Mzobe <104601010+NkanyisoMz@users.noreply.github.com>
2026-05-04 14:23:31 +0200

    fix: ensure membership role is correctly assigned using enum value

---
+++ app/controllers/teams_controller.rb
@@ -0,0 +1,34 @@
+class TeamsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @teams = current_user.teams
+  end
+
+  def new
+    @team = Team.new
+  end
+
+  def create
+    @team = Team.new(team_params)
+
+    if @team.save
+      # create membership as admin
+      Membership.create!(
+        user: current_user,
+        team: @team,
+        role: Membership.roles[:admin]
+      )
+
+      redirect_to teams_path, notice: "Team created successfully"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def team_params
+    params.require(:team).permit(:name)
+  end
+end
