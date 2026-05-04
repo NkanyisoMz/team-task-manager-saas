faf3895c8609fca56a69e878c138b0dd4c703fff
Nkanyiso Mzobe <104601010+NkanyisoMz@users.noreply.github.com>
2026-05-04 15:05:59 +0200

    feat: implement projects flow scoped to teams with secure access control

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,36 @@
+class ProjectsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_team
+
+  def index
+    @projects = @team.projects
+  end
+
+  def new
+    @project = @team.projects.new
+  end
+
+  def create
+    @project = @team.projects.new(project_params)
+
+    if @project.save
+      redirect_to team_projects_path(@team), notice: "Project created"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @project = @team.projects.find(params[:id])
+  end
+
+  private
+
+  def set_team
+    @team = current_user.teams.find(params[:team_id])
+  end
+
+  def project_params
+    params.require(:project).permit(:name, :description)
+  end
+end
