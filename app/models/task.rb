7ef68aa73d0063ae6a46dd38070f87840067bf2c
Nkanyiso Mzobe <104601010+NkanyisoMz@users.noreply.github.com>
2026-05-12 16:20:59 +0200

    Add task status workflow

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,7 +3,7 @@ class Task < ApplicationRecord
   belongs_to :assignee, class_name: "User", optional: true
   has_many :comments
 
-  enum status: { todo: 0, in_progress: 1, done: 2 }
+  enum status: { todo: 0, in_progress: 1, completed: 2 }
 
   validates :title, presence: true
 end
