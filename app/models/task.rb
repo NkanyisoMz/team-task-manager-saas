67153d963666d52648ccfb8491dc476f346fa5a1
Nkanyiso Mzobe <104601010+NkanyisoMz@users.noreply.github.com>
2026-07-23 01:45:49 +0200

    Colour-code task status dropdowns

--- app/models/task.rb
+++ app/models/task.rb
@@ -6,4 +6,18 @@ class Task < ApplicationRecord
   enum status: { todo: 0, in_progress: 1, completed: 2 }
 
   validates :title, presence: true
+
+  def status_classes
+    case status
+    when "todo"
+      "bg-gray-100 text-gray-700 border-gray-300"
+    when "in_progress"
+      "bg-blue-100 text-blue-700 border-blue-300"
+    when "completed"
+      "bg-green-100 text-green-700 border-green-300"
+    else
+      "bg-gray-100 text-gray-700 border-gray-300"
+    end
+  end
+
 end
